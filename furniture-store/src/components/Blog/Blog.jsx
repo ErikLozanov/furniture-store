148e46ca6769740d6bdf99268d46791995d8fd8a
Erik Lozanov <wolfierecordss@gmail.com>
2023-09-20 11:53:23 +0300

    Fixed picture sizes.

--- furniture-store/src/components/Blog/Blog.jsx
+++ furniture-store/src/components/Blog/Blog.jsx
@@ -2,8 +2,7 @@ import { BlogSection } from "./BlogSection/BlogSection";
 
 
 export const Blog = ({blogs}) => {
-    console.log(blogs);
-    console.log(blogs[0]);
+
     return (
         <>
             <BlogSection blogs={blogs} />
