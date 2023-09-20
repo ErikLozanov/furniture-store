61b0676d9ddbfea64f4ccdb0f62f8249be794c1d
Erik Lozanov <wolfierecordss@gmail.com>
2023-09-20 10:31:38 +0300

    Update ProductSection.jsx

--- furniture-store/src/components/Home/ProductSection/ProductSection.jsx
+++ furniture-store/src/components/Home/ProductSection/ProductSection.jsx
@@ -6,6 +6,16 @@ export const ProductSection = ({furnitures}) => {
     return (
         <div className="product-section">
   <div className="container">
+  <div className="row mb-5">
+      <div className="col-md-6">
+        <h2 className="section-title">Top Sellers</h2>
+      </div>
+      <div className="col-md-6 text-start text-md-end">
+        <Link to="/shop" className="more">
+          View All Furnitures
+        </Link>
+      </div>
+    </div>
     <div className="row">
       {slicedFurnitures.map((furniture)=> (FurnitureTemplate(furniture)))}
     </div>
